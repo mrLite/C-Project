d193bb9e0224eb97f35d68741301d246026ab111
Juuso Lehtinen <jupelehtinen@gmail.com>
2011-10-31 12:06:33 +0200

    added settings file id parser

--- read_data.c
+++ read_data.c
@@ -6,24 +6,42 @@ void read_data(FILE* fp, double* width, double* height, double* grid_size, int* 
   char string[30];
   char id[15];
   double num;
+  int len;
   
   fseek(fp, 0, SEEK_SET); //Set to stream start
 
   while(fgets(string, sizeof(string), fp) != NULL){
     if(string[0] != '#'){
       sscanf(string, "%[^0-9] %lf", id, &num);
-      
-      
-      if(id == "width")
-	*width = num;
-      if(id == "height")
-	*height = num;
-      if(id == "grid_size")
-	*grid_size = num;
-      if(id == "temp_left")
-	*temp_left = num;
 
+      //Parse setting identifier
+      len = (int)(strlen(id));
+     
+      if(parse_id(id, len, "width ") == 1)
+	
+      if(parse_id(id, len, "height ") == 1)
+	
+      if(parse_id(id, len, "grid_size ") == 1)
+	
+      if(parse_id(id, len, "temp_left ") == 1)
+	
+      if(parse_id(id, len, "temp_right ") == 1)
+	
+      if(parse_id(id, len, "temp_bottom ") == 1)
+	
+      if(parse_id(id, len, "temp_top ") == 1)
+	
     }
   }
+}
+
+int parse_id(char* string, int len, char* comp){
+  int ret = 1;
   
+  for(int i = 0; i < len; i++){
+    if(string[i] != comp[i])
+      ret = 0;
+  }
+
+  return ret;
 }
--- read_data.h
+++ read_data.h
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifndef __READ_DATA_H__
 #define __READ_DATA_H__
 
 void read_data(FILE* fp, double* width, double* height, double* grid_size, int* temp_left, int* temp_top, int* temp_right, int* temp_bottom);
+
+int parse_id(char* string, int len, char* comp);
 
 #endif
