1a766870ea2853206cb0aff31abfd67b82f0fce2
Markus Korpinen <markus.korpinen@gmail.com>
2011-11-02 17:11:03 +0200

    Major differences to approach for init_temp and added also new function

--- iter_temp.h
+++ iter_temp.h
@@ -1,15 +1,16 @@
 #ifndef __ITER_TEMP_H__
 #define __ITER_TEMP_H__
 
-/*typedef struct {
-	int myint;
-	char* mystring;
-} Data;*/
+typedef struct{
+	double t;
+}Temp;
 
-double average_temp(double* t[], int w_size, int h_size);
+Temp** initialize_temp(int w, int h, int temp_left, int temp_top, int temp_right, int temp_bottom);
 
-void iterate_temp(double* t1[], double* t2[], int w_size, int h_size);
+double average_temp(Temp** t, int w_size, int h_size);
 
-int compare_temp(double* t1[], double* t2[], int w_size, int h_size, double e);
+void iterate_temp(Temp** t1, Temp** t2, int w_size, int h_size);
+
+int compare_temp(Temp** t1, Temp** t2, int w_size, int h_size, double e);
 
 #endif
